c9ceb5aa82eaf82094d232e61b09481b3ad93bcc
2290421599@qq.com <20031023z>
2022-06-25 23:31:56 +0800

    docs(git): 新增myhome.vue文件

---
+++ src/views/MyHome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>新增数据</h1>
+    <h2>dsadsadasdsadasdasdas</h2>
+    <h3>dsadasdasdas</h3>
+    <p>dasdasdas大家好</p>
+    <b>dsadasdasd</b>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'myhome',
+  components: {}
+}
+</script>
