8cc1d9caf51b83384b2b80a8fb19701535c544f6
2290421599@qq.com <20031023z>
2022-06-25 21:26:26 +0800

    添加文件到git仓库

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>新增数据</h1>
+    <h2>dsadsadasdsadasdasdas</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
